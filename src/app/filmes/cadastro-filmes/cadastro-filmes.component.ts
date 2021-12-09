27e1a2f0390321e94798b8268cced46296212af2
nicktheodoro <nicolastheodoro@gmail.com>
2021-12-09 09:26:26 -0300

    adicionando validações ao formulário

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "dio-cadastro-filmes",
@@ -7,14 +7,44 @@ import { FormBuilder, FormGroup } from "@angular/forms";
   styleUrls: ["./cadastro-filmes.component.scss"],
 })
 export class CadastroFilmesComponent implements OnInit {
-  options: FormGroup;
+  cadastro: FormGroup;
 
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: "auto",
+  get f() {
+    return this.cadastro.controls;
+  }
+
+  ngOnInit(): void {
+    this.cadastro = this.fb.group({
+      titulo: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(256),
+        ],
+      ],
+      urlFoto: ["", [Validators.minLength(10)]],
+      dtLancamento: ["", [Validators.required]],
+      descricao: [""],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDB: ["", [Validators.minLength(10)]],
+      genero: ["", [Validators.required]],
     });
+  }
+
+  salvar(): void {
+    this.cadastro.markAllAsTouched();
+
+    if (this.cadastro.invalid) {
+      return;
+    }
+
+    alert("SUCESSO!!\n\n" + JSON.stringify(this.cadastro.value, null, 4));
+  }
+
+  reiniciarForm(): void {
+    this.cadastro.reset();
   }
 }
