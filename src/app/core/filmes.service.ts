ab2b83007414f1ba3209702ed8862f0297048c73
nicktheodoro <nicolastheodoro@gmail.com>
2021-12-14 13:12:36 -0300

    listagem de resultados e melhoria de performance

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,7 +1,11 @@
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+
+import { ConfigParams } from "../shared/models/config-params";
 import { Filme } from "../shared/models/filme";
+
+import { ConfigParamsService } from "./config-params.service";
 
 const url = "http://localhost:3000/filmes/";
 
@@ -9,13 +13,15 @@ const url = "http://localhost:3000/filmes/";
   providedIn: "root",
 })
 export class FilmesService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigParamsService
+  ) {}
 
-  listar(pagina: number, qtdPagina: number): Observable<Filme[]> {
-    let httpParams = new HttpParams();
-    httpParams = httpParams.set("_page", pagina.toString());
-    httpParams = httpParams.set("_limit", qtdPagina.toString());
-    return this.http.get<Filme[]>(url, { params: httpParams });
+  listar(config: ConfigParams): Observable<Filme[]> {
+    const configParams = this.configService.configrarParamentros(config);
+
+    return this.http.get<Filme[]>(url, { params: configParams });
   }
 
   salvar(filme: Filme): Observable<Filme> {
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,6 +1,11 @@
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { FilmesService } from "./../../core/filmes.service";
 import { Component, OnInit } from "@angular/core";
+
+import { debounceTime } from 'rxjs/operators';
+
 import { Filme } from "src/app/shared/models/filme";
+import { ConfigParams } from "src/app/shared/models/config-params";
 
 @Component({
   selector: "dio-listagem-filmes",
@@ -8,13 +13,57 @@ import { Filme } from "src/app/shared/models/filme";
   styleUrls: ["./listagem-filmes.component.scss"],
 })
 export class ListagemFilmesComponent implements OnInit {
-  readonly qtdPagina = 4;
-  filmes: Filme[] = [];
-  pagina = 0;
+  readonly qtdPagina = 8;
+  readonly semFoto =
+    "https://www.termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg";
 
-  constructor(private filmesService: FilmesService) {}
+  config: ConfigParams = {
+    pagina: 0,
+    limite: 8,
+  };
+  filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
+
+  constructor(private filmesService: FilmesService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [""],
+      genero: [""],
+    });
+
+    this.filtrosListagem
+      .get("texto")
+      .valueChanges
+      .pipe(debounceTime(400))
+      .subscribe((valor: string) => {
+        this.config.pesquisa = valor;
+        this.resetarConsulta();
+      });
+
+    this.filtrosListagem.get("genero").valueChanges.subscribe((val: string) => {
+      this.config.campo = { tipo: "genero", valor: val };
+      this.resetarConsulta();
+    });
+
+    this.generos = [
+      "Ação",
+      "Aventura",
+      "Comédia",
+      "Documentário",
+      "Drama",
+      "Faroeste",
+      "Fantasia",
+      "Ficção científica",
+      "Filmes de guerra",
+      "Mistério",
+      "Musical",
+      "Romance",
+      "Terror",
+      "Thriller",
+    ];
+
     this.listarFilmes();
   }
 
@@ -23,11 +72,17 @@ export class ListagemFilmesComponent implements OnInit {
   }
 
   private listarFilmes(): void {
-    this.pagina++;
+    this.config.pagina++;
 
     this.filmesService
-      .listar(this.pagina, this.qtdPagina)
-      .subscribe((filmes: Filme[]) => (this.filmes.push(...filmes)));
+      .listar(this.config)
+      .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void {
+    this.config.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
   }
 
   open() {}
